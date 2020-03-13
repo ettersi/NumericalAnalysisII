2dd2c7dcf2ce067dedde75e738c3a9717186de51
Simon Etter <ettersi@nus.edu.sg>
2020-03-13 14:38:59 +0800

    Add solutions to lab 5

---
+++ exercises/lab05_explicit_runge_kutta_solution.jl
@@ -0,0 +1,90 @@
+using PyPlot
+
+function cannonball_f(y,D,g)
+    x1,x2,dx1,dx2 = y
+    v = sqrt(dx1^2 + dx2^2)
+    return [dx1,dx2, -D*v*dx1, -D*v*dx2-g]
+end
+
+function euler_step(f,y0,t)
+    return y0 + f(y0)*t
+end
+
+function midpoint_step(f,y0,t)
+    f0 = f(y0)
+    f1 = f(y0+f0*t/2)
+    return y0 + f1*t
+end
+
+function ssprk3_step(f,y0,t)
+    f0 = f(y0)
+    f1 = f(y0+f0*t)
+    f2 = f(y0+f0*t/4+f1*t/4)
+    return y0 + f0*t/6 + f1*t/6 + f2*2t/3
+end
+
+function integrate(f,y0,T,n,step)
+    y = Vector{typeof(y0)}(undef,n)
+    y[1] = y0
+    for i = 2:n
+        y[i] = step(f,y[i-1],T/(n-1))
+    end
+    return y
+end
+
+function split(y)
+    x1 = [y[i][1] for i = 1:length(y)]
+    x2 = [y[i][2] for i = 1:length(y)]
+    dx1 = [y[i][3] for i = 1:length(y)]
+    dx2 = [y[i][4] for i = 1:length(y)]
+    return x1,x2,dx1,dx2
+end
+
+function plot_trajectory()
+    D = 5.0
+    g = 1.0
+    y0 = Float64[0,0,1,2]
+    T = 2.0
+
+    n = 100
+    step = euler_step
+    y = integrate(
+        y->cannonball_f(y,D,g),
+        y0, T, n, step
+    )
+    x1,x2,_ = split(y)
+    clf()
+    plot(x1,x2)
+    axis("equal")
+    xlabel(L"x_1")
+    ylabel(L"x_2")
+    display(gcf())
+end
+
+function convergence()
+    f = y->y^2
+    y0 = 1.0
+    T = 0.5
+    y = t-> y0/(1-y0*t)
+
+    clf()
+    n = round.(Int, 10.0.^LinRange(0,3,30))
+    for (name,step) in (
+        ("Euler", euler_step),
+        ("midpoint", midpoint_step),
+        ("SSPRK3", ssprk3_step),
+    )
+        error = [begin
+            ỹ = integrate(f,y0,T,n, step)
+            abs(y(T) - ỹ[end])
+        end for n in n]
+        loglog(n, error, label=name)
+    end
+    loglog(n, inv.(n), "k--")
+    loglog(n, inv.(n).^2, "k-.")
+    loglog(n, inv.(n).^3, "k:")
+    legend(loc="best")
+    xlabel(L"n")
+    ylabel(L"|\tilde y(T) - y(T)|")
+    display(gcf())
+end
